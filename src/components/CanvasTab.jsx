aa6ff5c02d931fb74a1e994d577aa19da5a750c6
Rishabh-Prajapti-329 <rishuk7300@gmail.com>
2025-07-04 23:44:38 +0530

    redo style

--- src/components/CanvasTab.jsx
+++ src/components/CanvasTab.jsx
@@ -4,7 +4,7 @@ const CanvasTab = ({ pageNumber, totalBoards, goPrev, goNext, addBoard, removeBo
 
 
     return (
-        <div style={{ position: "fixed", top: 100, left: 88, background: "rgb(225, 223, 223)", borderRadius: "4mm", padding: "0.4rem 0.7rem" }}>
+        <div style={{ position: "fixed", bottom: 70, left: 88, background: "rgb(225, 223, 223)", borderRadius: "4mm", padding: "0.4rem 0.7rem" }}>
 
             <div style={{ display: "flex", gap: "15px", justifyContent: "center", alignItems: "center" }}>
                 <button onClick={goPrev} disabled={pageNumber === 1} style={{ cursor: "pointer", color: "#A6A6A6", border: "none", background: "none" }}><i style={{ fontSize: "1.3rem" }} className="fa-regular fa-square-caret-left"></i></button>
